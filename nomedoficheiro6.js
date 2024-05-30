8be12e3255b8e0aa0d7c0cf7721ea6bb65f99e11
Rodrigo Martins <rmartins06112000@gmail.com>
2024-05-30 11:29:37 +0100

    file 6 comments and errors

--- nomedoficheiro6.js
+++ nomedoficheiro6.js
@@ -3,12 +3,13 @@
     const net = new brain.NeuralNetwork();
     
     net.train([
-        {input:{entrada1:0, entrada2:0}, output:{saida:0}},
+        {input:{entrada1:0, entrada2:0}, output:[0]}, //tentei ver se o output era o problema, não é
         {input:{entrada1:0, entrada2:1}, output:{saida:1}},
         {input:{entrada1:1, entrada2:0}, output:{saida:1}},
         {input:{entrada1:1, entrada2:1}, output:{saida:0}},
     ]);
 
+    // se derem debug e derem hover no net.run na janela vão até output, ele mostra 3 e o primeiro tem o output correto mas por alguma razão devolve o NaN dos outros outputs
     const output00 = parseFloat(
 
         net.run({entrada1:0, entrada2:0})
@@ -37,5 +38,5 @@
     console.log('0 xor 1: ' + output01);
     console.log('1 xor 0: ' + output10);
     console.log('1 xor 1: ' + output11);
-    
+
 // Guardem e testem agora no terminal node nomedoficheiro6
