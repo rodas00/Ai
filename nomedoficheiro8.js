3cba3dfeb89c2be7308d96ee346e7055109994bc
Rodrigo Martins <rmartins06112000@gmail.com>
2024-05-30 11:59:39 +0100

    file 8

---
+++ nomedoficheiro8.js
@@ -0,0 +1,23 @@
+// construindo a a variável de configuração num ficheiro nomedoficheiro8.js
+const brain = require('brainjs');
+const config = {
+binaryThresh: 0.5,
+hiddenLayers: [3],
+activation: 'sigmoid',
+leakyReluAlpha: 0.01,
+inputSize: 2,
+inputRange: 2,
+outputSize: 1,
+learningRate: 0.01,
+decayRate:0.999
+};
+const net = new brain.NeuralNetwork(config);
+net.train([
+{input:[0,0], output:[0]},
+{input:[0,1], output:[1]},
+{input:[1,0], output:[1]},
+{input:[1,1], output:[0]}
+]);
+const output = net.run([0,1]);
+console.log(output)
+// Guardem e testem a rede, como node nomedoficheiro8
